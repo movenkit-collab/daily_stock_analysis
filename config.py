2af2643f16f945292e8153c4f8e4152ba78c7286
movenkit-collab <movenkit@gmail.com>
2026-01-17 15:17:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Config:
     """
     
     # === 自选股配置 ===
-    stock_list: List[str] = field(default_factory)
+    stock_list: List[str] = field(default_factory=list)
     # === 飞书云文档配置 ===
     feishu_app_id: Optional[str] = None
     feishu_app_secret: Optional[str] = None
